52f41bdba1699bba4376b2d7e5331489ce7e21f4
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-07-20 22:35:21 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,14 +15,10 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/^[A-Z]/)
-end
-
-def first_word_capitalized_and_ends_with_punctuation?(text)
-  if text.match(/^[A-Z].*\.$/) == nil
-    return false
+  if text.match(/^[A-Z].*\.$/)
+    true
   else
-    return true
+    
   end
 end
 
